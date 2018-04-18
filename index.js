ba2c1b16eda6fd84eb47bdd14b27cd3b16a928de
olaph wagner <zxkuqyb@gmail.com>
2018-04-18 15:50:31 -0400

    clean up

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 function avatarAction(args) {
     const random_name = require("node-random-name")
-    svgString = "default";
-    getImage(function(result) {
-      return { name: random_name(), image: result }
-    })
-}
-
-function getImage(callback) {
     const minifig = require("minifig")
-    minifig.makeSVG(function(stuff) {
-      callback(stuff);
-    })
-}    
+    svgString = "default";
+    
+    return new Promise(function(resolve, reject) {
+      minifig.makeSVG(function(stuff) {
+        if (stuff) {
+          svgString = stuff;
+          resolve({ name : random_name(), image: stuff});
+        }
+        else {
+          reject(stuff);
+        }
+      });
+    });
+    
+    // promise.then(function(result) {
+      // svgString = result;
+      // return { name: random_name(), image: svgString};
+    // }, function(err) {
+      // console.log(err);
+    // });
+  
+    // return { name: random_name(), image: svgString }
+}
 
 exports.main = avatarAction;
