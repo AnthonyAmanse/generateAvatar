eb780ca1d580c076626112de1080a5c00d10ef93
olaph wagner <zxkuqyb@gmail.com>
2018-04-20 08:23:02 -0400

    change name generator to totro

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 function avatarAction(args) {
-    const random_name = require("node-random-name")
+    const totro = require("totro")
     const minifig = require("minifig")
     
     return new Promise(function(resolve, reject) {
       minifig.makeSVG(function(stuff) {
         if (stuff) {
           svgString = stuff;
-          resolve({ name : random_name(), image: stuff});
+          fullname = totro.RandomName(2,5) + " " + totro.RandomName(6,8);
+          resolve({ name : fullname, image: stuff});
         }
         else {
           reject(stuff);
         }
       });
     });
+    
 }
 
 exports.main = avatarAction;
